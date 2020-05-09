de1157eeb95b35e25e2803627e2773b9b6728ac1
samantadearaujo <samantadearaujo@gmail.com>
2020-05-09 23:08:32 +0100

    lab run program net core in linux

---
+++ App-Info-Year-Born/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             System.Console.WriteLine("What's your name? ");
+            String nome = System.Console.ReadLine();            
+            System.Console.WriteLine("Now, Can you tell me how old you are? ");
+            Int32 idade = Int32.Parse(System.Console.ReadLine());
+            Int32 year = DateTime.Now.Year - idade;
+            Console.WriteLine($"Hello World!, {nome}, and you born in {year}");
+            System.Console.WriteLine("hum...I'm not sure, Please, Can you tell what month you born?");
+            Int32 month = Int32.Parse(System.Console.ReadLine());
+            System.Console.WriteLine("hum...I'm not sure, Please, Can you tell what day you born?");
+            Int32 day = Int32.Parse(System.Console.ReadLine());
+
+            if(DateTime.Now.Day <= day && DateTime.Now.Month <= month)
+            {
+               year = year - 1; 
+               System.Console.WriteLine($"Ok. You born in {year}");
+            }
+            else
+            {
+                System.Console.WriteLine($"I'm right. You born in {year}");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # lab-net-lab
+Create new console 
+#dotnet new console -o App-Info-Year-Born -n console 
+
+<h2>What's the code to do?</h2>
+*********:~/github/csharp/lab-net-lab/App-Info-Year-Born$ dotnet run
+What's your name? 
+SAMANTA
+Now, Can you tell me how old you are? 
+20
+Hello World!, SAMANTA, and you born in 2000
+hum...I'm not sure, Please, Can you tell what month you born?
+6
+hum...I'm not sure, Please, Can you tell what day you born?
+21
+Ok. You born in 1999
